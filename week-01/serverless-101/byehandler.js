f4201216bd7f50480df703df6a8177e1b057a0fe
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-05-06 19:24:05 +0200

    Module 2: Building API with API Gateway and Lambda - inital commit

--- week-01/serverless-101/byehandler.js
+++ week-01/serverless-101/byehandler.js
@@ -1,10 +1,10 @@
 "use strict";
 
-module.exports.hello = async (event) => {
+module.exports.bye = async (event) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: "hello world",
+      message: "goodbye",
     }),
   };
 
